7209a6f58f5d0f3149040ef9eafdb6fa5fe3a84c
AjayDhami <ajay08dhami18@gmail.com>
2024-11-01 21:59:54 +0100

    Wiremock maven dependency added to test synchronous method call from order-service to inventory-service

--- order-service/src/test/java/com/ajay/microservices/order/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/ajay/microservices/order/OrderServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package com.ajay.microservices.order;
 
+import com.ajay.microservices.order.stubs.InventoryClientStub;
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,13 +8,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Import;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
 //@Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
 	@ServiceConnection
@@ -41,6 +43,7 @@ class OrderServiceApplicationTests {
                 }
                 """;
 
+		InventoryClientStub.stubInventoryCall("iphone_15", 1);
 
 		var responseBodyString = RestAssured.given()
 				.contentType("application/json")
