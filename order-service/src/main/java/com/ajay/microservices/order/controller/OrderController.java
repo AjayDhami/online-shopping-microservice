73d9b9536ea9d6bffeca30c4f58894037e503b00
AjayDhami <ajay08dhami18@gmail.com>
2024-11-01 21:29:02 +0100

    Spring Cloud OpenFeign implemented in order-service to allow order-service to make synchronous call to inventory-service

--- order-service/src/main/java/com/ajay/microservices/order/controller/OrderController.java
+++ order-service/src/main/java/com/ajay/microservices/order/controller/OrderController.java
@@ -22,5 +22,4 @@ public class OrderController {
         return "Order Placed Successfully";
     }
 
-
 }
--- order-service/src/main/java/com/ajay/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/ajay/microservices/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.ajay.microservices.order.service;
 
+import com.ajay.microservices.order.client.InventoryClient;
 import com.ajay.microservices.order.dto.OrderRequest;
 import com.ajay.microservices.order.model.Order;
 import com.ajay.microservices.order.repository.OrderRepository;
@@ -12,20 +13,28 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     @Autowired
-    public OrderService(OrderRepository orderRepository) {
+    public OrderService(OrderRepository orderRepository, InventoryClient inventoryClient) {
         this.orderRepository = orderRepository;
+        this.inventoryClient = inventoryClient;
     }
 
     public void placeOrder(OrderRequest orderRequest) {
-        Order order  = Order.builder()
-                .orderNumber(UUID.randomUUID().toString())
-                .skuCode(orderRequest.skuCode())
-                .price(orderRequest.price())
-                .quantity(orderRequest.quantity())
-                .build();
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
 
-        orderRepository.save(order);
+        if (isProductInStock) {
+            Order order = Order.builder()
+                    .orderNumber(UUID.randomUUID().toString())
+                    .skuCode(orderRequest.skuCode())
+                    .price(orderRequest.price())
+                    .quantity(orderRequest.quantity())
+                    .build();
+
+            orderRepository.save(order);
+        } else {
+            throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is out of stock");
+        }
     }
 }
