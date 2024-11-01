30cff8c50aef5cf3dabe55a5edda2192d66284ff
AjayDhami <ajay08dhami18@gmail.com>
2024-11-01 22:27:24 +0100

    API gateway implemented

---
+++ api-gateway/src/main/java/com/ajay/microservice/gateway/routes/Routes.java
@@ -0,0 +1,35 @@
+package com.ajay.microservice.gateway.routes;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+import static org.springframework.web.servlet.function.RequestPredicates.path;
+
+@Configuration
+public class Routes {
+
+    @Bean
+    public RouterFunction<ServerResponse> productServiceRoute() {
+        return route("product_service")
+                .route(path("/api/product"), http("http://localhost:8080"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> orderServiceRoute() {
+        return route("order_service")
+                .route(path("/api/order"), http("http://localhost:8081"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceRoute() {
+        return route("inventory_service")
+                .route(path("/api/inventory"), http("http://localhost:8082"))
+                .build();
+    }
+}
